2c591055577b8a9034662a72b72f37e03259bc3b
Peet Brits <peetbrits@gmail.com>
2026-09-18 19:06:19 +0200

    Unresolved reference 'Icons'.

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -7,11 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.practice.ui.theme.PracticeTheme
+
+// Tutorial: https://www.youtube.com/watch?v=EqCvUETekjk&list=PLQkwcJG4YTCT1LkjokmzZUFFyFVVWPuKk
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticeTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+
+                        // TODO. TopAppBar. Should I be using experimental features?
+
+                        @OptIn(ExperimentalMaterial3Api::class)
+                        TopAppBar(
+                            title = {
+                                Text(text = "My notes")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { }) {
+                                    Icon(
+
+                                        // TODO. Unresolved reference 'Icons'.
+
+                                        imageVector = Icons.Default.ArrowBack,
+                                        contentDescription = "Go back"
+                                    )
+                                }
+                            }
+                        )
+                    }
                 ) { innerPadding ->
                     LazyColumn(
                         modifier = Modifier
